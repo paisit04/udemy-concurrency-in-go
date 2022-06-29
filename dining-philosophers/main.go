b2380e22e40c417586dd108ac472b74aacefbe8d
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2022-06-29 01:17:30 -0700

    Section 4: A Classic Problem: The Dining Philosophers

---
+++ dining-philosophers/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
+
+// constants
+const hunger = 3
+
+// variables - philsophers
+var philosophers = []string{"Plato", "Socrates", "Aristotle", "Pascal", "Locke"}
+var wg sync.WaitGroup
+var sleepTime = 1 * time.Second
+var eatTime = 2 * time.Second
+var thinkTime = 1 * time.Second
+var orderFinished []string
+var orderMutex sync.Mutex
+
+func diningProblem(philosopher string, leftFork, rightFork *sync.Mutex) {
+	defer wg.Done()
+
+	// print a message
+	fmt.Println(philosopher, "is seated.")
+	time.Sleep(sleepTime)
+
+	for i := hunger; i > 0; i-- {
+		fmt.Println(philosopher, " is hungry.")
+		time.Sleep(sleepTime)
+
+		// lock both forks
+		leftFork.Lock()
+		fmt.Printf("\t%s picked up the fork to his left.\n", philosopher)
+		rightFork.Lock()
+		fmt.Printf("\t%s picked up the fork to his right.\n", philosopher)
+
+		// print a message
+		fmt.Println(philosopher, "has both forks, and is eating.")
+		time.Sleep(eatTime)
+
+		// give the philosopher some time to think
+		fmt.Println(philosopher, "is thinking")
+		time.Sleep(thinkTime)
+
+		// unlock the mutexes
+		rightFork.Unlock()
+		fmt.Printf("\t%s put down the fork on his right.\n", philosopher)
+		leftFork.Unlock()
+		fmt.Printf("\t%s put down the fork on his left.\n", philosopher)
+		time.Sleep(sleepTime)
+	}
+
+	// print out done message
+	fmt.Println(philosopher, "is satisfied.")
+	time.Sleep(sleepTime)
+
+	fmt.Println(philosopher, "has left the table.")
+	orderMutex.Lock()
+	orderFinished = append(orderFinished, philosopher)
+	orderMutex.Unlock()
+}
+
+func main() {
+	// print intro
+	fmt.Println("The Dining Philosophers Problem")
+	fmt.Println("-------------------------------")
+
+	wg.Add(len(philosophers))
+
+	forkLeft := &sync.Mutex{}
+
+	// spawn one goroutine for each philosopher
+	for i := 0; i < len(philosophers); i++ {
+		// create a mutex for the right fork
+		forkRight := &sync.Mutex{}
+
+		// call a goroutine
+		go diningProblem(philosophers[i], forkLeft, forkRight)
+
+		forkLeft = forkRight
+	}
+
+	wg.Wait()
+
+	fmt.Println("The table is empty.")
+	fmt.Println("-----------------------------")
+	fmt.Printf("Order finished: %s\n", strings.Join(orderFinished, ", "))
+}
